0e5fd690df9146e5cbec293e08346620a7def135
Danielggz <dggz1993@gmail.com>
2026-02-26 12:12:32 +0000

    Cloud fix

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,5 @@ pm2 stop example_app
 #Change directory into folder of the application
 cd ExampleApplication/
 #Install dependencies
-
 #Start the application
 pm2 start ./bin/www --name example_app
