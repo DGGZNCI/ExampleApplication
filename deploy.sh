d852338ab2586f86b8e20e96634797d1ac3ac608
Danielggz <dggz1993@gmail.com>
2026-03-03 10:39:07 +0000

    Updated deployment to install pm2

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #Environment setup
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 nvm install --lts
+npm install -g pm2
 #Stop current application running
 pm2 stop example_app
 #Change directory into folder of the application
